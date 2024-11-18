9236d87d7a434ae838bcd7939b1395fd0a887a2b
kim2haerin <yedidiahh@gmail.com>
2024-11-18 10:52:17 -0600

    circle

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// Project Title
+// Your Name
+// Date
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(mouseX, mouseY, 100);
+}
