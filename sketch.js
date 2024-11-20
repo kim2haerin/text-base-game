5959a4bd6481745ac5ab24e3009f584403af4b2a
yedidiah <yedidiahh@gmail.com>
2024-11-19 22:38:48 -0600

    putting the title of the game in the start window

--- sketch.js
+++ sketch.js
@@ -1,14 +1,6 @@
 // Project Title
 // Your Name
 
-document.addEventListener('DOMContentLoaded', function()){
-  let button = document.querySelector('#start-button');
-  let input = document.querySelector('#name-input');
-  button.addEventListener('click'. function()){
-    
-  }
-  l  
-  }
 
 
 function setup() {
