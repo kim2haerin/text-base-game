32271d63b8af2bb14c46c202a458b1585822d63f
kim2haerin <yedidiahh@gmail.com>
2024-12-02 11:03:07 -0600

    making the options buttons

--- sketch.js
+++ sketch.js
@@ -6,45 +6,36 @@ let playerHealth = 100;
 let PlayerInventory = [];
 let playerLevel = 1;
 let img;
+let storyText = ""; 
+let userInput = {}; 
+
 
 const textElement = document.getElementById("text");
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  startGame();
 }
 
-//function draw() {
-// background(220);
-//}
+function draw() {
+  //background(0);
+  textSize(16);
+  textAlign(LEFT);
+  fill("white");
+  text(storyText, 50, 150, width - 100);
+  
+}
 
 function startGame(){
-  console.log("welcome to adventure game!");
-  console.log("you find yourself in front a dark cave. Do you wanna go in?(Yes/No)");
-
-  let choice = prompt("Do you wanna go in?(Yes/No)");
-
-  if (choice.toLowerCase() === "yes"){
-    enterCave();
-  }
-  else{
-    console.log("maybe next time");
-    gameOver = true;
-  }
+  state = {};
+  showTextNode(1);
 }
 
-function enterCave(){
-  console.log("you are now inside the cave and you hear a strange noise");
-  console.log("there are two path in front of you. which way to go?(Left/Right)");
-  let choice = prompt("which way to go?(Left/Right)");
-
-  if(choice.toLowerCase() === "right"){
-    rightPath();
-  }
-  else if(choice.toLowerCase()==="left"){
-    leftPath();
-  }
+function showTextNode(textNodeIndex){
+  const textNode = textNodes.find(textNode => textNode.id === 
+    textNodeIndex);
+  textElement.innerText = textNode.text;
 }
 
-function RightPath(){
-  console.log("you take the right path and on your way you find a small knife");
-}
+
+
